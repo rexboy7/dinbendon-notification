3eb5b12ffedba2cc324600eda2997eda86f2578c
Rex Lee <rexboy@mozilla.com>
2014-06-24 16:23:29 +0800

    initial release

---
+++ js/dinbendon-notification.js
@@ -0,0 +1,103 @@
+var endHour = 16;
+var endMinute = 0;
+var remindPeriodInSecond = 28800;
+var remindIntervalInSecond = 3600;
+
+function init() {
+  chrome.storage.sync.get(
+    ['endHour', 'endMinute', 'remindPeriodInSecond', 'remindIntervalInSecond'],
+    function(items) {
+      if(!items.endHour) {
+        chrome.storage.sync.set({
+          endHour: endHour,
+          endMinute: endMinute,
+          remindPeriodInSecond: remindPeriodInSecond,
+          remindIntervalInSecond: remindIntervalInSecond
+        }, function() {});
+      } else {
+        endHour = items.endHour;
+        endMinute = items.endMinute;
+        remindPeriodInSecond = items.remindPeriodInSecond;
+        remindIntervalInSecond = items.remindIntervalInSecond;
+      }
+      setNextAlarm(true);
+      chrome.alarms.onAlarm.addListener(popNotification);
+    });
+}
+
+function getEndTime() {
+  var time = new Date();
+  var yy = time.getFullYear();
+  var mm = time.getMonth();
+  var dd = time.getDate();
+  return new Date(yy, mm, dd, endHour, endMinute);
+}
+
+function getStartTime() {
+  // Start reminding from 20 minutes ago
+  return new Date(getEndTime().getTime() - remindPeriodInSecond * 1000);
+}
+
+function getNextDayStartTime() {
+  return new Date(getStartTime().getTime() + 86400000);
+}
+
+function setNextAlarm(initialize) {
+  var time = new Date();
+  var endTime = getEndTime();
+  var startTime = getStartTime();
+  if (time < startTime) {
+    chrome.alarms.create('dinbendon', {when: startTime.getTime()});
+  } else if (time > startTime && time < endTime) {
+    if(initialize) {
+      popNotification({name: 'dinbendon'});
+    }
+    if (endTime - time >= remindIntervalInSecond * 1000) {
+      chrome.alarms.create('dinbendon',
+                              {delayInMinutes: remindIntervalInSecond / 60});
+    } else {
+      setNextDayAlarm();
+    }
+  } else {
+    setNextDayAlarm();
+  }
+}
+
+function setNextDayAlarm() {
+  chrome.alarms.create('dinbendon', {when: getNextDayStartTime().getTime()});
+
+  var opt = {
+    type: 'basic',
+    title: '訂便當',
+    message: '將於明天提醒您訂便當。',
+    iconUrl: 'style/icons/128.png'
+  };
+  chrome.notifications.clear('nextday', function() {});
+  chrome.notifications.create('nextday', opt, function() {});
+}
+
+function popNotification(alarm) {
+  if(alarm.name == 'dinbendon') {
+    if (chrome.notifications) {
+      var opt = {
+        type: 'basic',
+        title: '訂便當',
+        message: '提醒您，訂便當的時間到了，請記得下單。按我跳轉',
+        iconUrl: 'style/icons/128.png',
+        buttons: [{title: '停止提醒'}]
+      };
+      chrome.notifications.clear('dinbendon', function() {});
+      chrome.notifications.create('dinbendon', opt, function() {});
+      setNextAlarm();
+      chrome.notifications.onClicked.addListener(function() {
+        window.open('http://dinbendon.net');
+        setNextDayAlarm();
+      });
+      chrome.notifications.onButtonClicked.addListener(function() {
+        setNextDayAlarm();
+      });
+    }
+  }
+}
+
+init();
---
+++ js/options.js
@@ -0,0 +1,89 @@
+/* globals chrome */
+
+(function options() {
+'use strict';
+
+  var hourInput = document.getElementById('hour');
+  var minInput = document.getElementById('min');
+  var remindbeforeInput = document.getElementById('remindbefore');
+  var remindeveryInput = document.getElementById('remindevery');
+  var updateBtn = document.getElementById('update');
+  var output = document.getElementById('output');
+  var frequencyGroup = document.getElementById('frequencyGroup');
+  var frequencySet;
+
+  function onFrequencyChange(evt) {
+    remindbeforeInput.disabled = remindeveryInput.disabled =
+                                                (evt.target.value != 'custom');
+    switch(evt.target.value) {
+      case 'early':
+        remindbeforeInput.value = 480;
+        remindeveryInput.value = 60;
+        break;
+      case 'late':
+        remindbeforeInput.value = 30;
+        remindeveryInput.value = 3;
+        break;
+    }
+  }
+
+  function setFrequencyGroup(grpName) {
+    var elements = document.getElementsByName('frequency');
+    var i, checked;
+    elements[0].checked = true;
+    checked = 0;
+    for(i = 0; i < elements.length; i++) {
+      if(elements[i].value == grpName) {
+        elements[i].checked = true;
+        checked = i;
+      }
+    }
+    onFrequencyChange({target: elements[checked]});
+  }
+
+  function getFrequencyGroup() {
+    var elements = document.getElementsByName('frequency');
+    for(var i = 0; i < elements.length; i++) {
+      if(elements[i].checked) {
+        return elements[i].value;
+      }
+    }
+  }
+
+  function saveSettings() {
+    var opt = {
+      endHour: hourInput.value,
+      endMinute: minInput.value,
+      remindPeriodInSecond: remindbeforeInput.value * 60,
+      remindIntervalInSecond: remindeveryInput.value * 60,
+      frequencyGroup: getFrequencyGroup()
+    };
+    console.log(opt);
+    chrome.storage.sync.set(opt, function() {
+      output.textContent = '設定已儲存';
+      setTimeout(cleanOutput, 3000);
+      chrome.extension.getBackgroundPage().init();
+    });
+  }
+
+  function cleanOutput() {
+    output.textContent = '';
+  }
+
+  function init() {
+    chrome.storage.sync.get(
+      ['endHour', 'endMinute', 'remindPeriodInSecond', 'remindIntervalInSecond',
+       'frequencyGroup'],
+      function(items) {
+        hourInput.value = items.endHour;
+        minInput.value = items.endMinute;
+        remindbeforeInput.value = items.remindPeriodInSecond / 60;
+        remindeveryInput.value = items.remindIntervalInSecond / 60;
+        setFrequencyGroup(items.frequencyGroup);
+      });
+    updateBtn.addEventListener('click', saveSettings);
+    frequencyGroup.addEventListener('change', onFrequencyChange);
+  }
+
+  init();
+})();
